709ce7afa124d8982057a277f8dcee9128b852b6
Joel <joeljossie@gmail.com>
2022-01-27 20:46:40 -0700

    added constants, updated demo map

--- open_window.py
+++ open_window.py
@@ -1,20 +1,31 @@
 import arcade
 
 # Constants
+
+# Window Options
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
+
 # Scaling
 CHARACTER_SCALING = 0.5
-# TILE_SCALING = 0.9142857142857 # using old 70x70px sprites
-# COIN_SCALING = 0.9142857142857 # using old 70x70px sprites
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
+
 # Physics
 PLAYER_MOVEMENT_SPEED = 10
 PLAYER_ACCELERATION = 0.9
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
+
+# Layer Names
+LAYER_NAME_PLAYER = "player"
+LAYER_NAME_ITEMS = "items"
+LAYER_NAME_BOXES = "boxes"
+LAYER_NAME_PLATFORMS = "platforms"
+LAYER_NAME_DEATH = "death"
+LAYER_NAME_BACKGROUND = "background"
+
 # Game Logic
 COIN_VALUE = 5
 
@@ -46,6 +57,7 @@ class MyGame(arcade.Window):
 
         # Game Logic
         self.score = 0
+        self.max_score = 0
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
@@ -69,39 +81,12 @@ class MyGame(arcade.Window):
         # Initialize Scene with tilemap
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
-        # # Create Sprite Lists
-        # self.scene.add_sprite_list("Player")
-        # self.scene.add_sprite_list("Walls", use_spatial_hash=True)
-        # self.scene.add_sprite_list("Coins")
-
         # Create Sprites
         player_sprite_path = "sprites/PNG/Players/128x256/Green/alienGreen_stand.png"
         self.player_sprite = arcade.Sprite(player_sprite_path, CHARACTER_SCALING)
         self.player_sprite.center_x = 128
         self.player_sprite.center_y = 256
-        self.scene.add_sprite("player", self.player_sprite)
-        #
-        # # Generate Wall Tiles
-        # for x in range(0, 2500, 64):
-        #     wall = arcade.Sprite("Sprites/Tiles/Ground Tiles/Grass/grassMid.png", TILE_SCALING)
-        #     wall.center_x = x
-        #     wall.center_y = 32
-        #     self.scene.add_sprite("Walls", wall)
-        #
-        # # Generate Boxes
-        # coordinate_list = [[512, 96], [256, 96], [768, 96]]
-        # for coordinate in coordinate_list:
-        #     crate = arcade.Sprite("Sprites/Tiles/box.png", TILE_SCALING)
-        #     crate.position = coordinate
-        #     self.scene.add_sprite("Walls", crate)
-        #
-        # # Generate Coins
-        # # Use a loop to place some coins for our character to pick up
-        # for x in range(128, 2500, 192):
-        #     coin = arcade.Sprite("Sprites/Items/Coins/coinGold.png", COIN_SCALING)
-        #     coin.center_x = x
-        #     coin.center_y = 96
-        #     self.scene.add_sprite("Coins", coin)
+        self.scene.add_sprite(LAYER_NAME_PLAYER, self.player_sprite)
 
         # Initialize Physics Engine
         self.physics_engine = arcade.PhysicsEnginePlatformer(
@@ -110,19 +95,20 @@ class MyGame(arcade.Window):
 
         # Game Logic
         self.score = 0
+        self.max_score = COIN_VALUE * len(self.scene.get_sprite_list(LAYER_NAME_ITEMS))
 
     def on_key_press(self, key: int, modifiers: int):
         """Called whenever a key is pressed."""
 
         if key in [arcade.key.UP, arcade.key.W]:
             self.up_pressed = True
-        elif key in [arcade.key.DOWN, arcade.key.S]:
+        if key in [arcade.key.DOWN, arcade.key.S]:
             self.down_pressed = True
-        elif key in [arcade.key.LEFT, arcade.key.A]:
+        if key in [arcade.key.LEFT, arcade.key.A]:
             self.left_pressed = True
-        elif key in [arcade.key.RIGHT, arcade.key.D]:
+        if key in [arcade.key.RIGHT, arcade.key.D]:
             self.right_pressed = True
-        elif key == arcade.key.SPACE:
+        if key == arcade.key.SPACE:
             self.space_pressed = True
 
     def on_key_release(self, key: int, modifiers: int):
@@ -130,13 +116,13 @@ class MyGame(arcade.Window):
 
         if key in [arcade.key.LEFT, arcade.key.A]:
             self.left_pressed = False
-        elif key in [arcade.key.RIGHT, arcade.key.D]:
+        if key in [arcade.key.RIGHT, arcade.key.D]:
             self.right_pressed = False
-        elif key in [arcade.key.UP, arcade.key.W]:
+        if key in [arcade.key.UP, arcade.key.W]:
             self.up_pressed = False
-        elif key in [arcade.key.DOWN, arcade.key.S]:
+        if key in [arcade.key.DOWN, arcade.key.S]:
             self.down_pressed = False
-        elif key == arcade.key.SPACE:
+        if key == arcade.key.SPACE:
             self.space_pressed = False
 
     def update_player_speed(self):
@@ -151,6 +137,8 @@ class MyGame(arcade.Window):
 
         if self.physics_engine.can_jump() and (self.up_pressed or self.space_pressed):
             self.player_sprite.change_y = PLAYER_JUMP_SPEED
+
+        print(self.player_sprite.change_x)
 
     def apply_acceleration(self, is_right):
         """Gradually increase the velocity of a particular axis."""
@@ -217,13 +205,31 @@ class MyGame(arcade.Window):
 
         # Check for coin collisions
         coin_hit_list = arcade.check_for_collision_with_list(
-            self.player_sprite, self.scene["items"]
+            self.player_sprite, self.scene[LAYER_NAME_ITEMS]
         )
-
         for coin in coin_hit_list:
             # Remove the coin
             coin.remove_from_sprite_lists()
             self.score += COIN_VALUE
+            if self.has_won():
+                self.win()
+
+        # Check for death
+        death_hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.scene[LAYER_NAME_DEATH]
+        )
+        if len(death_hit_list) > 0:
+            self.die()
+
+    def die(self):
+        self.setup()
+
+    def has_won(self) -> bool:
+        return self.score >= self.max_score
+
+    def win(self):
+        print("you win")
+        pass
 
 
 def main():
