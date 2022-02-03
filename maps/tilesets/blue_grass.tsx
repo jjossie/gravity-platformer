90e3b8a8376e53ab993be752d68778ab6ccf3fd0
Joel <joeljossie@gmail.com>
2022-02-03 01:02:07 -0700

    added tilesets

---
+++ maps/tilesets/blue_grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="blue_grass" tilewidth="1024" tileheight="1024" tilecount="1" columns="1">
+ <image source="../../sprites/PNG/Backgrounds/blue_grass.png" width="1024" height="1024"/>
+</tileset>
---
+++ maps/tilesets/sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="sheet" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="../sprites/platformer-extendedtiles-0/PNG Cake/Spritesheet/sheet.png" width="512" height="512"/>
+</tileset>
